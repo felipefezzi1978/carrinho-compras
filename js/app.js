62608787da0b140be457ae55f9013d498ee8ebfa
FELIPE ALVES FEZZI <86987347+felipefezzi1978@users.noreply.github.com>
2025-08-16 16:18:53 -0300

    Implementação de controle entrada de produto válido

--- js/app.js
+++ js/app.js
@@ -1,41 +1,48 @@
 let totalGeral;
 limpar();
 
+
 function adicionar() {
-    //recuperar valores: nome do produto, quantidade e valor
     let produto = document.getElementById('produto').value;
-    let nomeProduto = produto.split('-')[0];
-    let valorUnitario = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade').value;
 
-    //validar se a quantidade é maior que zero
-    if (quantidade <=0 ) {
-        alert('A quantidade deve ser maior que zero!');
+
+    // Verificar se o produto selecionado é válido
+    if (!produto || produto.trim() === "") {
+        alert("Selecione um produto válido.");
         return;
     }
-    //calcular o preço, o nosso subproduto
-    preço = quantidade * valorUnitario;
- 
-    //adicionar no carrinho
-    let carrinho = document.getElementById('lista-produtos');
-        carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
-          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preço}</span>
-        </section>`;
-        
-    
-    //atualizar o valor total
-    totalGeral = totalGeral + preço;
-    let campoTotal = document.getElementById('valor-total');
-    campoTotal.textContent = `R$ ${totalGeral.toFixed(2)}`;
-    document.getElementById('quantidade').value = 0;
 
+
+    // Verificar se a quantidade inserida é válida
+    if (isNaN(quantidade) || quantidade <= 0) {
+        alert("Insira uma quantidade válida.");
+        return;
+    }
+
+
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = parseFloat(produto.split('R$')[1]);
+    let preco = quantidade * valorUnitario;
+
+
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R${preco}</span>
+  </section>`;
+
+
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
 }
+
 
 
 
 function limpar() {
     totalGeral = 0;
     document.getElementById('lista-produtos').innerHTML = '';
-    document.getElementById('valor-total').textContent = 'R$ 0,00';
-
+    document.getElementById('valor-total').textContent = 'R$0';
 }
